97c60c71ab1f1568b998464dc57ed9a2693135a5
Sam Izzo <sam@horsedrawngames.com>
2015-06-14 23:25:54 +1000

    Bumped version number.

--- NShaderVS/NShaderVersion.cs
+++ NShaderVS/NShaderVersion.cs
@@ -2,7 +2,7 @@ namespace NShader
 {
     public class NShaderVersion
     {
-        public const string VERSION = "1.51";
+        public const string VERSION = "1.52";
         public const string ASSEMBLY_VERSION = VERSION + ".0.0";
         public const string ASSEMBLY_FILE_VERSION = ASSEMBLY_VERSION;
     }
