6e4195ea9c55e7849c477de963059a03d374c723
Sam Izzo <sam@horsedrawngames.com>
2017-03-16 22:01:11 +1100

    Updated nshader to vs2017.

--- NShaderVS/NShaderVersion.cs
+++ NShaderVS/NShaderVersion.cs
@@ -2,7 +2,7 @@ namespace NShader
 {
     public class NShaderVersion
     {
-        public const string VERSION = "2.2";
+        public const string VERSION = "2.3";
         public const string ASSEMBLY_VERSION = VERSION + ".0.0";
         public const string ASSEMBLY_FILE_VERSION = ASSEMBLY_VERSION;
     }
