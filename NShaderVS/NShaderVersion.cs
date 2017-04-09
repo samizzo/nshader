da592e182c88cd1f94df46671c79396bd62d12e2
Sam Izzo <sam@horsedrawngames.com>
2017-04-09 23:25:21 +1000

    Bumped NShader version.

--- NShaderVS/NShaderVersion.cs
+++ NShaderVS/NShaderVersion.cs
@@ -2,7 +2,7 @@ namespace NShader
 {
     public class NShaderVersion
     {
-        public const string VERSION = "2.3";
+        public const string VERSION = "2.4";
         public const string ASSEMBLY_VERSION = VERSION + ".0.0";
         public const string ASSEMBLY_FILE_VERSION = ASSEMBLY_VERSION;
     }
